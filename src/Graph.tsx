289de94426712d2a3ef1d25dc36be871da247bf1
rahulholla97 <rahul@de-haze.com>
2024-07-06 18:21:19 +0530

    patch-2

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -19,42 +19,55 @@ class Graph extends Component<IProps, {}> {
   }
 
   componentDidMount() {
-    // Get element from the DOM.
-    const elem = document.getElementsByTagName('perspective-viewer')[0] as unknown as PerspectiveViewerElement;
+    const elem = document.getElementsByTagName('perspective-viewer')[0] as PerspectiveViewerElement;
 
     const schema = {
-      stock: 'string',
-      top_ask_price: 'float',
-      top_bid_price: 'float',
-      timestamp: 'date',
+        price_abc: 'float',
+        price_def: 'float',
+        ratio: 'float',
+        timestamp: 'date',
+        upper_bound: 'float',
+        lower_bound: 'float',
+        trigger_alert: 'float',
     };
 
-    if (window.perspective && window.perspective.worker()) {
-      this.table = window.perspective.worker().table(schema);
+    if (window.perspective && window.perspective.worker) {
+        this.table = window.perspective.worker().table(schema);
     }
     if (this.table) {
-      // Load the `table` in the `<perspective-viewer>` DOM reference.
-      elem.load(this.table);
-      elem.setAttribute('view', 'y_line');
-      elem.setAttribute('column-pivots', '["stock"]');
-      elem.setAttribute('row-pivots', '["timestamp"]');
-      elem.setAttribute('columns', '["top_ask_price"]');
-      elem.setAttribute('aggregates', JSON.stringify({
-        stock: 'distinctcount',
-        top_ask_price: 'avg',
-        top_bid_price: 'avg',
-        timestamp: 'distinct count',
-      }));
+        elem.load(this.table);
     }
+
+    elem.setAttribute('view', 'y_line');
+    elem.setAttribute('row-pivots', '["timestamp"]');
+    elem.setAttribute('columns', '["ratio", "upper_bound", "lower_bound", "trigger_alert"]');
+    elem.setAttribute('aggregates', JSON.stringify({
+        price_abc: 'avg',
+        price_def: 'avg',
+        ratio: 'avg',
+        timestamp: 'distinct count',
+        upper_bound: 'avg',
+        lower_bound: 'avg',
+        trigger_alert: 'avg'
+    }));
   }
 
   componentDidUpdate() {
     if (this.table) {
-      this.table.update(
-        DataManipulator.generateRow(this.props.data),
-      );
+        this.table.update([
+            this.props.data.map((el: any) => {
+                return {
+                    price_abc: el.price_abc,
+                    price_def: el.price_def,
+                    ratio: el.ratio,
+                    timestamp: el.timestamp,
+                    upper_bound: el.upper_bound,
+                    lower_bound: el.lower_bound,
+                    trigger_alert: el.trigger_alert,
+                };
+            }),
+        ]);
     }
-  }
 }
 
 export default Graph;
