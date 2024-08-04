2fa3d1713f61512a58753be9dba27013d16c4e23
UJJWAL SRIVASTAVA <119431039+ujjwalsri1@users.noreply.github.com>
2024-08-04 19:32:51 +0530

    Update DataManipulator.ts

--- src/DataManipulator.ts
+++ src/DataManipulator.ts
@@ -1,20 +1,33 @@
 import { ServerRespond } from './DataStreamer';
 
 export interface Row {
-  stock: string,
-  top_ask_price: number,
+  price_abc: number,
+  price_def: number,
+  ratio: number,
   timestamp: Date,
+  upper_bound: number,
+  lower_bound: number,
+  trigger_alert: number | undefined,
 }
-
 
 export class DataManipulator {
-  static generateRow(serverResponds: ServerRespond[]) {
-    return serverResponds.map((el: any) => {
-      return {
-        stock: el.stock,
-        top_ask_price: el.top_ask && el.top_ask.price || 0,
-        timestamp: el.timestamp,
-      };
-    })
+  static generateRow(serverResponds: ServerRespond[]): Row[] {
+    const priceABC = serverResponds.find(d => d.stock === 'ABC')?.top_ask.price;
+    const priceDEF = serverResponds.find(d => d.stock === 'DEF')?.top_ask.price;
+    const ratio = priceABC && priceDEF ? priceABC / priceDEF : 0;
+    const upperBound = 1.05;
+    const lowerBound = 0.95;
+    const triggerAlert = (ratio > upperBound || ratio < lowerBound) ? ratio : undefined;
+
+    return serverResponds.map((el: any) => ({
+      price_abc: el.stock === 'ABC' ? el.top_ask.price : priceABC || 0,
+      price_def: el.stock === 'DEF' ? el.top_ask.price : priceDEF || 0,
+      ratio,
+      timestamp: el.timestamp,
+      upper_bound: upperBound,
+      lower_bound: lowerBound,
+      trigger_alert: triggerAlert,
+    }));
   }
 }
+
